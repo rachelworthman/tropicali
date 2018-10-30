469112d208af5f9ec5f80018e4963cd85409525e
Rachel Worthman <rachelworthman@gmail.com>
2018-10-30 14:33:10 -0700

    github deploy script added

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,8 @@ var browserSync = require('browser-sync').create()
 
 var imagemin = require('gulp-imagemin')
 
+var ghpages = require('gh-pages')
+
 sass.compiler = require('node-sass')
 
 gulp.task('sass', function() {
@@ -59,6 +61,12 @@ gulp.task('watch', function() {
     gulp.watch('src/css/app.scss', ['sass'])
     gulp.watch('src/fonts/*', ['fonts'])
     gulp.watch('src/img/*', ['images'])
+})
+
+
+
+gulp.task('deploy', function() {
+    ghpages.publish('dist')
 })
 
 
